dfea47d83d6af6bda4f24ae67d1dfb47f25892a0
al222410966-droid <al222410966@gmail.com>
2025-11-29 17:07:11 -0600

    cambio despues de borrar

---
+++ Fronted/src/components/themes/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+
+  azul: "#2196F3",
+  naranja: "#FF9800", 
+  verde: "#4CAF50",
+  amarillo: "#FFC107",
+  rojo: "#F44336",
+  gris_fondo: "#F5F5F5",
+  blanco: "#FFFFFF",
+  negro_texto: "#212121",
+  gris_texto: "#757575"
+
+}
